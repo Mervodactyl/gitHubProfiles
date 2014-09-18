b1d5bc1a753b5732027420de2b98df313e12ed58
Mervodactylgit config --global user.name Mervodactylgit config --global user.name Mervodactyl <merve.silk@gmail.com>
2014-09-18 15:16:37 +0100

    sorted out my mustache

--- MerveGHP/interface.js
+++ MerveGHP/interface.js
@@ -1,14 +1,19 @@
-      $(document).ready(function(){
-        $('#add-profile').on('submit', function(e){
-          e.preventDefault();
-          var url = 'https://api.github.com/users/' + $('#username').val() + '?access_token=73cb7811fcc34c769d0585da0f8f50d8bb0f61bc';
+$(document).ready(function(){
+  $('#add-profile').on('submit', function(e){
+    e.preventDefault();
+    var url = 'https://api.github.com/users/' + $('#username').val() + '?access_token=73cb7811fcc34c769d0585da0f8f50d8bb0f61bc';
 
-          var userData = $.get(url, function(user) {
-            $('<article class="profile"><section class="pic"><img src="' + user.avatar_url + '"></section><section class="statistics"><h3><a href="https://github.com/' + user.login + '">' + user.login + '</a></h3><strong>Repos: ' + user.public_repos + '</strong><strong>Followers: ' + user.followers + '</strong></section><br clear="all"></article>').appendTo('.profile-container');
-          }).fail(function(){
-            alert('Could not find this user.');
-          }).always(function() {
-            $('#username').val('');
-          })
-        })
-      })
+    var userData = $.get(url, function(user) {
+      var template = $('#profile-template').html();
+      $('.profile-container').append(Mustache.render(template, user));
+
+
+      // $('<article class="profile"><section class="pic"><img src="' + user.avatar_url + '"></section><section class="statistics"><h3><a href="https://github.com/' + user.login + '">' + user.login + '</a></h3><strong>Repos: ' + user.public_repos + '</strong><strong>Followers: ' + user.followers + '</strong></section><br clear="all"></article>').appendTo('.profile-container');
+
+    }).fail(function(){
+      alert('Could not find this user.');
+    }).always(function() {
+      $('#username').val('');
+    })
+  })
+})
